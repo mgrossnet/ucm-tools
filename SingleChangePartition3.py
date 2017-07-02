96e13cd4542fd40fa7e61e2bbee9171a3be45067
Michael Gross <mgrossnet@gmail.com>
2017-07-02 13:09:19 -0400

    Updated with args

--- SingleChangePartition3.py
+++ SingleChangePartition3.py
@@ -1,8 +1,7 @@
-# This is the Python 3 version of the script.
-
 from suds.client import Client
 from suds.xsd.doctor import Import
 from suds.xsd.doctor import ImportDoctor
+from optparse import OptionParser
 from getpass import getpass
 import os
 import sys
@@ -18,45 +17,90 @@ f = 0
 dnp = ""
 ip, user, pwd = '', '', ''
 exitcode = 0
-
-# Find out what version of CUCM we are talking to so we use the
-# correct version of the AXL toolkit.
-axlver = input("What Version of CUCM? 11.5, 10.5, etc. > ")
+cucmver = ''
+wsdl = ''
 
 
-# This block checks the path you are in and uses the axlsqltoolkit
-# under the path of the script location.
-fileDir = os.path.dirname(os.path.realpath('__file__'))
-rel_path = 'axlsqltoolkit/schema/' + str(axlver) + '/AXLAPI.wsdl'
-fullfilepath = os.path.join(fileDir, rel_path)
-# If it's Windows we need to fix the backslashes to forward slashes.
-normalizedfilepath = fullfilepath.replace('\\', '/')
-# Join the file path depending on the system
-if platform.system() == 'Windows':
-    wsdl = 'file:///' + normalizedfilepath
-else:
-    wsdl = 'file://' + normalizedfilepath
+
+def axltoolkit(axlver):
+    # This block checks the path you are in and uses the axlsqltoolkit
+    # under the path of the script location.
+    fileDir = os.path.dirname(os.path.realpath('__file__'))
+    rel_path = 'axlsqltoolkit/schema/' + str(axlver) + '/AXLAPI.wsdl'
+    fullfilepath = os.path.join(fileDir, rel_path)
+    # If it's Windows we need to fix the backslashes to forward slashes.
+    normalizedfilepath = fullfilepath.replace('\\', '/')
+    # Join the file path depending on the system
+    if platform.system() == 'Windows':
+        wsdl = 'file:///' + normalizedfilepath
+    else:
+        wsdl = 'file://' + normalizedfilepath
+    return wsdl
 
 
 
 def main():
-    global ip,user,pwd,client
-    ip = input("Please Enter the IP Address or Hostname of your CUCM > ")
-    user = input("Please Enter Your CUCM User ID > ")
-    pwd = getpass("Please Enter Your Password > ")
+    parser = OptionParser()
+    parser.add_option('-i', dest='host', help='Please specify UCM address.')
+    parser.add_option('-u', dest='user', help='Enter Username.')
+    parser.add_option('-p', dest='pwd', help='Enter Password.')
+    parser.add_option('-v', dest='ver', help='Enter Version. (10.0, 10.5, 11.0, 11.5)')
+    parser.add_option('-c', dest='currentpartition', help='Enter current partition')
+    parser.add_option('-n', dest='newpartition', help='Enter new partition')
+    (options, args) = parser.parse_args()
+    global ip, user, pwd, client, axlver, wsdl
+    if options.ver:
+        axlver = options.ver
+    else:
+        axlver = input("Please Enter the version of the CUCM cluster (10.0, 10.5, 11.0, 11.5) > ")
+    if options.host:
+        ip = options.host
+    else:
+        ip = input("Please Enter the IP Address or Hostname of your CUCM > ")
+    if options.user:
+        user = options.user
+    else:
+        user = input("Please Enter Your CUCM User ID > ")
+    if options.pwd:
+        pwd = options.pwd
+    else:
+        pwd = getpass("Please Enter Your Password > ")
     tns = 'http://schemas.cisco.com/ast/soap/'
     imp = Import('http://schemas.xmlsoap.org/soap/encoding/',
                  'http://schemas.xmlsoap.org/soap/encoding/')
     imp.filter.add(tns) 
     location = 'https://' + ip + ':8443/axl/'
+    wsdl = axltoolkit(axlver)
     try:
         client = Client(wsdl, location=location, faults=False,
                     plugins=[ImportDoctor(imp)], username=user, password=pwd)
     except:
-        print('Error with version or IP address of server. Please try again.')
+        print ("Error with version or IP address of server. Please try again.")
         sys.exit()
-    partold = input('Current Partition > ')
-    partnew = input('New Partition > ')
+    try:
+        verresp = client.service.getCCMVersion()
+    except:
+        print('Unknown Error. Please try again.')
+        sys.exit()
+    if verresp[0] == 401:
+        print('Authentication failure. Wrong username or password.')
+        sys.exit()
+    cucmver = verresp[1]['return'].componentVersion.version
+    cucmsplitver = cucmver.split('.')
+    cucmactualver = cucmsplitver[0] + '.' + cucmsplitver[1]
+    print('This cluster is version ' + cucmver)
+    if axlver != cucmactualver:
+        print('You chose the wrong version. The correct version is ') + cucmactualver
+        print('Please choose the correct version next time.')
+        sys.exit()
+    if options.currentpartition:
+        partold = options.currentpartition
+    else:
+        partold = input('Current Partition > ')
+    if options.newpartition:
+        partnew = options.newpartition
+    else:
+        partnew = input('New Partition > ')
     while exitcode != 1:
         dntochange = input('Number to be modified? Hit <CR> to exit. > ')
         if dntochange == '':
