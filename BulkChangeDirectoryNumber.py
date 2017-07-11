343599aabe52d28be492f24968b51a7465ab56bb
Michael Gross <mgrossnet@gmail.com>
2017-07-11 10:52:57 -0400

    Updated DN bulk

--- BulkChangeDirectoryNumber.py
+++ BulkChangeDirectoryNumber.py
@@ -1,8 +1,8 @@
 from suds.client import Client
 from suds.xsd.doctor import Import
 from suds.xsd.doctor import ImportDoctor
-from optparse import OptionParser
 from getpass import getpass
+from optparse import OptionParser
 import os
 import sys
 import platform
@@ -16,28 +16,27 @@ if hasattr(ssl, '_create_unverified_context'):
 f = 0
 dnp = ""
 ip, user, pwd = '', '', ''
-
-# Find out what version of CUCM we are talking to so we use the
-# correct version of the AXL toolkit.
-axlver = raw_input("What Version of CUCM? 11.5, 10.5, etc. > ")
-
-
-# This block checks the path you are in and uses the axlsqltoolkit
-# under the path of the script location.
-fileDir = os.path.dirname(os.path.realpath('__file__'))
-rel_path = 'axlsqltoolkit/schema/' + str(axlver) + '/AXLAPI.wsdl'
-fullfilepath = os.path.join(fileDir, rel_path)
-# If it's Windows we need to fix the backslashes to forward slashes.
-normalizedfilepath = fullfilepath.replace('\\', '/')
-# Join the file path depending on the system
-if platform.system() == 'Windows':
-    wsdl = 'file:///' + normalizedfilepath
-else:
-    wsdl = 'file://' + normalizedfilepath
+exitcode = 0
+cucmver = ''
+wsdl = ''
 
 
-def setcredentials(ip,user,pwd):
-    ip,user,pwd = ip,user,pwd
+
+def axltoolkit(axlver):
+    # This block checks the path you are in and uses the axlsqltoolkit
+    # under the path of the script location.
+    fileDir = os.path.dirname(os.path.realpath('__file__'))
+    rel_path = 'axlsqltoolkit/schema/' + str(axlver) + '/AXLAPI.wsdl'
+    fullfilepath = os.path.join(fileDir, rel_path)
+    # If it's Windows we need to fix the backslashes to forward slashes.
+    normalizedfilepath = fullfilepath.replace('\\', '/')
+    # Join the file path depending on the system
+    if platform.system() == 'Windows':
+        wsdl = 'file:///' + normalizedfilepath
+    else:
+        wsdl = 'file://' + normalizedfilepath
+    return wsdl
+
 
 def dnchange(dntochange, dnp, dnnew):
         resp = client.service.updateLine(pattern=dntochange, routePartitionName = dnp, newPattern=dnnew)
@@ -46,31 +45,68 @@ def dnchange(dntochange, dnp, dnnew):
         else:
             print ("Problem finding DN %s in Partition %s" % (dntochange,dnp))
 
+
 def main():
     parser = OptionParser()
-    parser.add_option('-f',dest='file',help='Please specify file name with extension.')
-    (options,args) = parser.parse_args()
-    global ip,user,pwd,client
-    ip = raw_input("Please Enter the IP Address or Hostname of your CUCM > ")
-    user = raw_input("Please Enter Your CUCM User ID > ")
-    pwd = getpass("Please Enter Your Password > ")
-    setcredentials(ip,user,pwd)
+    parser.add_option('-f', dest='file', help='Please specify file name with extension.')
+    parser.add_option('-i', dest='host', help='Please specify UCM address.')
+    parser.add_option('-u', dest='user', help='Enter Username.')
+    parser.add_option('-p', dest='pwd', help='Enter Password.')
+    parser.add_option('-v', dest='ver', help='Enter Version.')
+    (options, args) = parser.parse_args()
+    global ip, user, pwd, client, axlver, wsdl
+    if options.ver:
+        axlver = options.ver
+    else:
+        axlver = raw_input("Please Enter the version of the CUCM cluster (10.0, 10.5, 11.0, 11.5) > ")
+    if options.host:
+        ip = options.host
+    else:
+        ip = raw_input("Please Enter the IP Address or Hostname of your CUCM > ")
+    if options.user:
+        user = options.user
+    else:
+        user = raw_input("Please Enter Your CUCM User ID > ")
+    if options.pwd:
+        pwd = options.pwd
+    else:
+        pwd = getpass("Please Enter Your Password > ")
     tns = 'http://schemas.cisco.com/ast/soap/'
     imp = Import('http://schemas.xmlsoap.org/soap/encoding/',
                  'http://schemas.xmlsoap.org/soap/encoding/')
     imp.filter.add(tns) 
     location = 'https://' + ip + ':8443/axl/'
+    wsdl = axltoolkit(axlver)
     try:
         client = Client(wsdl, location=location, faults=False,
                     plugins=[ImportDoctor(imp)], username=user, password=pwd)
     except:
         print "Error with version or IP address of server. Please try again."
         sys.exit()
+    try:
+        verresp = client.service.getCCMVersion()
+    except:
+        print('Unknown Error. Please try again.')
+        sys.exit()
+    if verresp[0] == 401:
+        print('Authentication failure. Wrong username or password.')
+        sys.exit()
+    cucmver = verresp[1]['return'].componentVersion.version
+    cucmsplitver = cucmver.split('.')
+    cucmactualver = cucmsplitver[0] + '.' + cucmsplitver[1]
+    print('This cluster is version ' + cucmver)
+    if axlver != cucmactualver:
+        print('You chose the wrong version. The correct version is ') + cucmactualver
+        print('Please choose the correct version next time.')
+        sys.exit()
     with open(options.file, 'r') as f:
         for dn_dest in f.readlines():
+            if dn_dest == '\n':
+                continue
             if ',' in dn_dest:
                 dn_dest = dn_dest.split(',')
                 dnchange(dn_dest[0],dn_dest[1],dn_dest[2].rstrip())
+
 
 
 if __name__ == '__main__':
