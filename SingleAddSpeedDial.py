9ed17da1e21e19066f25c771cc8d69a270c80f0c
Michael Gross <mgrossnet@gmail.com>
2018-02-06 16:46:52 -0500

    Update partition change with SSO option and add speed dial

--- SingleAddSpeedDial.py
+++ SingleAddSpeedDial.py
@@ -1,25 +1,24 @@
 from suds.client import Client
 from suds.xsd.doctor import Import
 from suds.xsd.doctor import ImportDoctor
-from suds.plugin import MessagePlugin
-import base64
-import argparse
 from getpass import getpass
+import argparse
 import os
 import sys
 import platform
-from datetime import datetime
-
 # This is monkey patching SSL to not validate the certificate
 import ssl
 if hasattr(ssl, '_create_unverified_context'):
     ssl._create_default_https_context = ssl._create_unverified_context
-# End of SSL monkey patch
+# End of SSL monkeypatch
 
 # Variable assignments
+f = 0
 ip, user, pwd = '', '', ''
+cucmver = ''
+wsdl = ''
 exitcode = 0
-loginfo = ''
+
 
 
 def axltoolkit(axlver):
@@ -38,18 +37,18 @@ def axltoolkit(axlver):
     return wsdl
 
 
+
 def main():
     parser = argparse.ArgumentParser(description='UCM Script Options')
     parser.add_argument('-i', dest='host', help='Please specify UCM address.')
     parser.add_argument('-u', dest='user', help='Enter Username.')
     parser.add_argument('-p', dest='pwd', help='Enter Password.')
     parser.add_argument('-v', dest='ver', help='Enter Version. (10.0, 10.5, 11.0, 11.5)')
-    parser.add_argument('-c', dest='currentpartition', help='Enter current partition')
-    parser.add_argument('-n', dest='newpartition', help='Enter new partition')
-    parser.add_argument('-a', dest='sso', help='SSO enabled "true" or "false" - "false is assumed"')
-    parser.add_argument('-l', dest='log', help='Log file name.')
+    parser.add_argument('-num', dest='sdin_num', help='Speed Dial Number to be added')
+    parser.add_argument('-index', dest='sdin_index', help='Speed Dial placement')
+    parser.add_argument('-label', dest='sdin_label', help='Speed Dial Label')
     options = parser.parse_args()
-    global ip, user, pwd, client, axlver, wsdl, loginfo
+    global ip, user, pwd, client, axlver, wsdl
     if options.ver:
         axlver = options.ver
     else:
@@ -66,40 +65,30 @@ def main():
         pwd = options.pwd
     else:
         pwd = getpass("Please Enter Your Password > ")
-    if options.sso:
-        ssocheck = options.sso
+    if options.sdin_num:
+        add_num = options.sdin_num
     else:
-        ssocheck = "false"
-    if options.log:
-        logfile = options.log + datetime.now().strftime('%Y-%m-%d-%H%M%S') + '.log'
+        add_num = raw_input("Please Enter Speed Dial number to add > ")
+    if options.sdin_label:
+        add_label = options.sdin_label
     else:
-        logfile = 'lastlog' + datetime.now().strftime('%Y-%m-%d-%H%M%S') + '.log'
+        add_label = raw_input("Please Enter Speed Dial label to add > ")
+    if options.sdin_index:
+        add_index = options.sdin_index
+    else:
+        add_index = raw_input("Please Enter Speed Dial position to add > ")
     tns = 'http://schemas.cisco.com/ast/soap/'
     imp = Import('http://schemas.xmlsoap.org/soap/encoding/',
                  'http://schemas.xmlsoap.org/soap/encoding/')
     imp.filter.add(tns) 
     location = 'https://' + ip + ':8443/axl/'
     wsdl = axltoolkit(axlver)
-    if ssocheck == "true":
-        base64string = base64.encodestring('%s:%s' % (user, pwd)).replace('\n', '')
-        authenticationHeader = {
-            "SOAPAction" : "ActionName",
-            "Authorization" : "Basic %s" % base64string
-        }
-        try:
-            client = Client(wsdl,location=location,faults=False,plugins=[ImportDoctor(imp)],
-                    headers=authenticationHeader)
-        except:
-            print "Error with version or IP address of server. Please try again."
-            sys.exit()
-
-    else:
-        try:
-            client = Client(wsdl, location=location, faults=False,
-                        plugins=[ImportDoctor(imp)], username=user, password=pwd)
-        except:
-            print "Error with version or IP address of server. Please try again."
-            sys.exit()
+    try:
+        client = Client(wsdl, location=location, faults=False,
+                    plugins=[ImportDoctor(imp)], username=user, password=pwd)
+    except:
+        print "Error with version or IP address of server. Please try again."
+        sys.exit()
     try:
         verresp = client.service.getCCMVersion()
     except:
@@ -116,32 +105,39 @@ def main():
         print('You chose the wrong version. The correct version is ') + cucmactualver
         print('Please choose the correct version next time.')
         sys.exit()
-    if options.currentpartition:
-        partold = options.currentpartition
-    else:
-        partold = raw_input('Current Partition > ')
-    if options.newpartition:
-        partnew = options.newpartition
-    else:
-        partnew = raw_input('New Partition > ')
+    new_sd = {'dirn':add_num,'label':add_label,'index':add_index}
     while exitcode != 1:
-        dntochange = raw_input('Number to be modified? Hit <CR> to exit. > ')
-        if dntochange == '':
-            with open(logfile, 'w') as f:
-                f.write(loginfo)
+        phone = raw_input('Phone to be modified? Hit <CR> to exit. > ')
+        if phone == '':
             break
-        current_line = client.service.getLine(pattern=dntochange, routePartitionName = partold)
-        resp = client.service.updateLine(pattern=dntochange, routePartitionName = partold, newRoutePartitionName=partnew)
-        if resp[0] == 200:
-            print ("Success - Changed DN %s to Partition %s." % (dntochange,partnew))
-            loginfo = loginfo + "\nSuccess - Changed DN %s to Partition %s." % (dntochange,partnew)
+        get_phone_resp = client.service.getPhone(name=phone)
+        try:
+            master_speeddial_list = get_phone_resp[1]['return'].phone.speeddials
+        except:
+            print('Phone not found. Please try again.')
+            continue
+        if master_speeddial_list == "":
+            speeddial_new = {'speeddial':[]}
+            speeddial_new['speeddial'].append(new_sd)
         else:
-            print ("Problem finding DN %s in Partition %s" % (dntochange,partold))
-            loginfo = loginfo + "\nProblem finding DN %s in Partition %s" % (dntochange,partold)
-            print resp
+            speeddial_new = {'speeddial':[]}
+            for existing_speeddials in master_speeddial_list[0]:
+                if existing_speeddials.index == add_index:
+                    speeddial_new['speeddial'].append(new_sd)
+                    continue
+                else:
+                    speeddial_new['speeddial'].append({'dirn':existing_speeddials.dirn, 'label':existing_speeddials.label, 'index':existing_speeddials.index})
+        update_phone_resp = client.service.updatePhone(name=phone, speeddials=speeddial_new)
+        if update_phone_resp[0] == 200:
+            print ("Success - Updated phone %s speed dial with %s." % (phone,add_num))
+            print ("Speed dials on this phone are: ") + str(speeddial_new)
+        else:
+            print ("Problem updating phone %s speed dial with %s" % (phone,add_num))
+            print update_phone_resp
 
 
 
 if __name__ == '__main__':
     main()
+
 
